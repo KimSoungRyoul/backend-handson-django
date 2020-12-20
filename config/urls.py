35c86da47c2d7c2ec2980b2a1dc9d5d292302a56
KimSoungRyoul <KimSoungRyoul@gmail.com>
2020-12-20 18:35:35 +0900

    20201220 작업

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularJSONAPIView, SpectacularRedocView, SpectacularSwaggerView, SpectacularYAMLAPIView
+from drf_spectacular.views import (
+    SpectacularJSONAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+    SpectacularYAMLAPIView,
+)
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet
+from chapter1_app.views import UserViewSet
 
 router = DefaultRouter()
 
@@ -11,25 +16,20 @@ router.register(r"users", UserViewSet, basename="user")
 
 
 urlpatterns = [
-
     path("", include(router.urls)),
-
     path("admin/", admin.site.urls),
-
-    # YOUR PATTERNS
-    path("api/docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
-    path("api/docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
-    # Optional UI:
+    # Open API 문서 소스코드 형태
+    path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
+    path("docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
+    # Open API Document UI 형태:
     path(
-        "api/docs/swagger/",
+        "docs/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema-json"),
         name="swagger-ui",
     ),
-
     path(
-        "api/docs/redoc/",
+        "docs/redoc/",
         SpectacularRedocView.as_view(url_name="schema-json"),
         name="redoc",
     ),
-
 ]
