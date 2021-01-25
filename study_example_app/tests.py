cebb4e29c68e5b12f81261a47c8fb93f4784dc20
KimSoungRyoul <KimSoungRyoul@gmail.com>
2021-01-25 09:26:06 +0900

    중간 작업물 저장

---
+++ study_example_app/tests.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class DjangoORMExampleTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='abc1234', password='pass1!', email='asdf@naver.com', first_name='김예제')
+        User.objects.create_user(username='bbb1234', password='pass1!', email='bbb@naver.com', first_name='최예제')
+
+    def test_example_01(self):
+
+        user_list = User.objects.filter(username='abc1234')
+
+        self.assertEqual(user_list[0].first_name, '김예제')
